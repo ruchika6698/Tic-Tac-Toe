da5ab6c6af1c40321239a2abc5f3fcd7443509d5
ruchika <ruchika6698@gmail.com>
2020-03-24 12:22:39 +0530

    Added:Assign Symbol to Player

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,3 +1,21 @@
 #!/bin/bash -x
 
-printf "Tic Tac Toe Game \n"
+printf "Welcome to Tic Tac Toe Game \n"
+
+function resetBoard(){
+	gameBoard=( 1 2 3 4 5 6 7 8 9 )
+	echo " ${gameBoard[0]} ${gameBoard[1]} ${gameBoard[2]} "
+	echo " ${gameBoard[3]} ${gameBoard[4]} ${gameBoard[5]} "
+	echo " ${gameBoard[6]} ${gameBoard[7]} ${gameBoard[8]} "
+}
+
+function assignSymbol(){
+	resetBoard
+	if [[ $((RANDOM%2)) -eq 1 ]]
+	then
+		printf "Player got 'X' Symbol \n"
+	else
+		printf "Player got 'O' Symbol \n"
+	fi
+}
+assignSymbol
